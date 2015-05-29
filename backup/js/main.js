6c4e4bfd88505546883a5905b8f8cc4272e16bff
Yunfei Li <dylannnnlee@gamil.com>
2015-05-29 10:06:16 +0200

    2.3.1 syn from server
    Commit a wrong one

---
+++ backup/js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+	$("form.searchInput div.upload").click(function () {
+		$("form.searchInput input.uploadBtn").click();
+	});
+	
+	$('[data-toggle="tooltip"]').tooltip();
+    
+    $('.conjugaisonList a').each(function () {
+        $(this).click(function () {
+            if ($(this).attr('aria-expanded') == "false") {
+                $(this).children('span.icon').addClass('fa-minus');
+            } else {
+                $(this).children('span.icon').removeClass('fa-minus');
+            }
+        });
+    });
+});
