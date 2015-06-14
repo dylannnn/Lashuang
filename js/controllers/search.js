abc4102f4e7e69e8b4b9e2a850e6df938abfbf92
Yunfei Li <dylannnnlee@gamil.com>
2015-06-15 00:31:26 +0200

    3.1.5 Daily update
    Nothing special

--- js/controllers/search.js
+++ js/controllers/search.js
@@ -1,14 +1,14 @@
 app.controller("searchController", ["$scope", "$rootScope", "$location", "$route", "FIREBASE_URL", "$firebaseArray", "pageLoad", "Upload", function ($scope, $rootScope, $location, $route, FIREBASE_URL, $firebaseArray, pageLoad, Upload) {
     //initial function running
     pageLoad.iniFunc();
+    
+    //Debug para
 	console.dir($route.current.params);
 	
-	
-	
-    
+    //tab feature
     $scope.selectedTab = 1;
 	
-	//Get query word
+	//Get query word Function. Return the string
 	function getQueryVal() {
 		var vars = {};
 		var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi,    
@@ -17,22 +17,28 @@ app.controller("searchController", ["$scope", "$rootScope", "$location", "$route
 		});
 		return vars;
 	}
-
-	$rootScope.queryWord = getQueryVal()["q"];
-	//console.log(queryWord);
-	
+    
+//===============================================
+	//$rootScope.queryWord = getQueryVal()["q"];
 	$rootScope.found = false;
-	console.log("1. $rootScope.found is " + $rootScope.found);
+//===============================================
+	console.log("1. $rootScope.found is " + $rootScope.found);//false
+    
+    //Search Word function. 
 	function searchWord () {
+        //Connect to firebase
 		var ref = new Firebase(FIREBASE_URL + "dictionary");
         var dictionary = $firebaseArray(ref);
 		
+        //Callback function. After loaded, then run this.
 		dictionary.$loaded().then(function(data) {
+            
 			console.log("Data length: " + data.length);
 			console.dir(data);
 			
 			$rootScope.queryWord = getQueryVal()["q"];
 			
+            //Create an array to compare with the query string
 			var words = [];
 			for (var i = 0; i < data.length; i++) {
 				var wordContent = data[i];
@@ -41,8 +47,11 @@ app.controller("searchController", ["$scope", "$rootScope", "$location", "$route
 			console.dir(words);
 			
 			if (words.indexOf($rootScope.queryWord) != -1) {
+                //if found
 				$rootScope.found = true;
 				console.log("2. $rootScope.found is " + $rootScope.found);
+                
+                //???
 				$scope.searchedWord = $rootScope.queryWord;
 				
 			} else {
@@ -51,7 +60,7 @@ app.controller("searchController", ["$scope", "$rootScope", "$location", "$route
 			
 			
 				
-	
+	//search. For in loop
 	function loadQueryWordContent () {
 		console.log("Loading...");
 		var ref = new Firebase(FIREBASE_URL + "dictionary");
@@ -72,35 +81,39 @@ app.controller("searchController", ["$scope", "$rootScope", "$location", "$route
 	}
 	searchWord ();
 	
+    
+    
     //Add New Word
-    $('.successAdded').hide();
-    $scope.addWord = function () {
-        console.log("Clicked!");
-        var ref = new Firebase(FIREBASE_URL);
-        var dictionary = ref.child('dictionary');
-
-        //var dictContent = 
-        dictionary.push({
-            word : $scope.word.newWord,
-            image : $scope.word.imageName
-        }, function (error) {
-            if (error) {
-                console.log('Push failed');
-            } else {
-                //$scope.newWord = '';
-                console.log('Push Successed');
-                $scope.clearInput();
-                $scope.added();
-            }
-        });
-    };
-	
+//    $('.successAdded').hide();
+//    $scope.addWord = function () {
+//        console.log("Clicked!");
+//        var ref = new Firebase(FIREBASE_URL);
+//        var dictionary = ref.child('dictionary');
+//
+//        //var dictContent = 
+//        dictionary.push({
+//            word : $scope.word.newWord,
+//            image : $scope.word.imageName
+//        }, function (error) {
+//            if (error) {
+//                console.log('Push failed');
+//            } else {
+//                //$scope.newWord = '';
+//                console.log('Push Successed');
+//                $scope.clearInput();
+//                $scope.added();
+//            }
+//        });
+//    };
+//	
+    // Put new word explanation to an array
     $scope._newWordExp = [];
+    
     
     $scope.addItemOne = function (index) {
         //$scope.itemOne = $scope._newWordExp;
         $scope._newWordExp.push({
-            word: $scope.word.newWordExp
+            content: $scope.word.newWordExp
         });
         console.dir($scope._newWordExp);
         $scope.word.newWordExp = '';
@@ -116,7 +129,7 @@ app.controller("searchController", ["$scope", "$rootScope", "$location", "$route
     $scope.addItemTwo = function (index) {
         //$scope.itemTwo = $scope._newWordCommonUsage;
         $scope._newWordCommonUsage.push({
-            word: $scope.word.newWordCommonUsage
+            content: $scope.word.newWordCommonUsage
         });
         console.dir($scope._newWordCommonUsage);
 		$scope.word.newWordCommonUsage = '';
@@ -131,7 +144,7 @@ app.controller("searchController", ["$scope", "$rootScope", "$location", "$route
     $scope.addItemThree = function (index) {
         //$scope.itemThree = $scope._newWordExamples;
         $scope._newWordExamples.push({
-            word: $scope.word.newWordExamples
+            content: $scope.word.newWordExamples
         });
         console.dir($scope._newWordExamples);
 		$scope.word.newWordExamples = '';
